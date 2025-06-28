8c9846f81c307fab732ef8ca04d4467c2782bdcf
three13solutions <admin@313s.com>
2025-06-28 16:37:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,6 @@ export default function App() {
     } catch (error) {
       console.error('Error:', error);
       setFormStatus('❌ Submission failed due to network error.');
-    }
   };
 
   return (
